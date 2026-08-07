c7fc7e3c98424cae2670b7a3ecb5194c47cad4ba
lukeblanc <86426535+lukeblanc@users.noreply.github.com>
2026-08-07 18:47:43 +0800

    Bump connection test build to v0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mossy.mayhemlive"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
